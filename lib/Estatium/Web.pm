b5cc0c9e10b45f409869ae7abe7196e51463a15f
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2013-05-18 20:37:12 +0400

    Authenticate all API requests except to /login

--- lib/Estatium/Web.pm
+++ lib/Estatium/Web.pm
@@ -1,6 +1,8 @@
 package Estatium::Web;
 use Mojo::Base 'Mojolicious';
 
+use Estatium::DB;
+
 # This method will run once at server start
 sub startup {
     my $self = shift;
@@ -11,6 +13,22 @@ sub startup {
     # Router
     my $r = $self->routes;
     $r->post('/user/login')->to('user#login');
+
+    my $auth = $r->bridge('/')->to(
+        cb => sub {
+            my $c = shift;
+
+            # Authenticate all API routes except /login
+            return 1
+                if $c->req->url->path eq '/login'
+                || $c->session->{token}
+                && Estatium::DB->schema->resultset('User')->find(
+                    { token => $c->session->{token} }
+                );
+            $self->render(json => { login => { failed => 1 } });
+            return;
+        }
+    );
 
     # Operations with estates
     $r->post('/estate')->to('estate#create');
