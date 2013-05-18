5d664220e3380ab7aa2662599ca2bd7795f3566e
Ilya Chesnokov <chesnokov.ilya@gmail.com>
2013-05-18 20:43:04 +0400

    Updated user's sample data

--- sql/sample/data.sql
+++ sql/sample/data.sql
@@ -1,3 +1,5 @@
+SET foreign_key_checks = 0;
+
 -- Roles
 DELETE FROM role;
 INSERT INTO role VALUES (1, 'Admin');
@@ -7,9 +9,9 @@ INSERT INTO role VALUES (3, 'User');
 -- Users
 -- Password: 'password'
 DELETE FROM user;
-INSERT INTO user VALUES (1, 'admin@example.com',   '212cb08fa2e7ee673020962d175685e2ea1c9f3e', 1, 'Crazy',  'Admin');
-INSERT INTO user VALUES (2, 'manager@example.com', '212cb08fa2e7ee673020962d175685e2ea1c9f3e', 2, 'Cool',   'Manager');
-INSERT INTO user VALUES (3, 'user@example.com',    '212cb08fa2e7ee673020962d175685e2ea1c9f3e', 3, 'Simple', 'User');
+INSERT INTO user VALUES (1, 'admin@example.com',   '212cb08fa2e7ee673020962d175685e2ea1c9f3e', 1, 'Crazy',  'Admin', NULL);
+INSERT INTO user VALUES (2, 'manager@example.com', '212cb08fa2e7ee673020962d175685e2ea1c9f3e', 2, 'Cool',   'Manager', NULL);
+INSERT INTO user VALUES (3, 'user@example.com',    '212cb08fa2e7ee673020962d175685e2ea1c9f3e', 3, 'Simple', 'User', NULL);
 
 -- Estates
 DELETE FROM estate;
